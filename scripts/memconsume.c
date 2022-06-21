00fc23f96fc81dc94c1331b804465f539dc60c14
AlexisMerienne <alexis.merienne@gmail.com>
2022-06-21 12:21:16 +0200

    add scritp

---
+++ scripts/memconsume.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main(){
+
+    while(0==0){
+        for (int k=0;k<100;k++){
+            int * n = malloc(10000*sizeof(int));    
+        }
+    }
+
+    return 0;
+}
